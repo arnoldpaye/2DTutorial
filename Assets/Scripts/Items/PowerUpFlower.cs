35525c631d2c6aa16dde167b1970cebb2a6c7eab
arnoldpaye <arnoldpayet@gmail.com>
2016-02-09 11:57:47 -0400

    items

---
+++ Assets/Scripts/Items/PowerUpFlower.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PowerUpFlower : Collectable {
+
+	public int itemID = 1;
+
+	override protected void OnCollect(GameObject target) {
+		var equipBehavior = target.GetComponent<Equip> ();
+
+		if (equipBehavior != null) {
+			equipBehavior.currentItem = itemID;
+		}
+	}
+}
