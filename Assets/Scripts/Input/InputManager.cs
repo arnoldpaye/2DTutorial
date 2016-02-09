42e5e3315895770f21a90ad877bb2fb54c0e9621
arnoldpaye <arnoldpayet@gmail.com>
2016-02-09 09:56:50 -0400

    initial commit

---
+++ Assets/Scripts/Input/InputManager.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum Buttons {
+	Right,
+	Left,
+	Up,
+	Down,
+	A,
+	B
+}
+
+public enum Condition {
+	GreaterThan,
+	LessThan
+}
+
+[System.Serializable]
+public class InputAxisState {
+	public string axisName;
+	public float offValue;
+	public Buttons button;
+	public Condition condition;
+
+	public bool value {
+		get {
+			var val = Input.GetAxis (axisName);
+
+			switch (condition) {
+			case Condition.GreaterThan:
+				return val > offValue;
+			case Condition.LessThan:
+				return val < offValue;
+			}
+			return false;
+		}
+	}
+}
+
+public class InputManager : MonoBehaviour {
+
+	public InputAxisState[] inputs;
+	public InputState inputState;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		foreach (var input in inputs) {
+			/*if (input.value) {
+				Debug.Log ("Input Value " + input.value);
+				Debug.Log ("Input Detected " + input.button);
+			}*/
+			inputState.SetButtonValue (input.button, input.value);
+		}
+	}
+}
